f2090125020b4af0abc88819ec505d874494ce2c
Henrik <squaad1993@gmail.com>
2021-12-25 00:22:40 +0100

    Update stock

--- stock/models.py
+++ stock/models.py
@@ -7,7 +7,6 @@ from django.db import models
 class Stock(models.Model):
     name = models.CharField(max_length=30)
     ticker = models.CharField(max_length=30)
-    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
