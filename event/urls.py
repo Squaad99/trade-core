22a82e4fe7dda2ec81b4a63e6be55499fb73aeba
Henrik <squaad1993@gmail.com>
2021-12-25 21:32:36 +0100

    change to every day for health check

--- event/urls.py
+++ event/urls.py
@@ -33,7 +33,7 @@ class TCoreScheduler:
             trade_suite_event = TradeSuiteEvent(name="Check order and transactions", custom_date=str(datetime.now(time_zone)))
             trade_suite_event.save()
 
-        schedule.every().monday.at("11:00").do(health_check_job)
+        schedule.every().day.at("11:00").do(health_check_job)
 
         schedule.every().monday.at("15:00").do(buy_and_sell_job)
         schedule.every().tuesday.at("15:00").do(buy_and_sell_job)
