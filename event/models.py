a467452fc23eed6fbd70edd9554709f1a5e9de65
Henrik <squaad1993@gmail.com>
2022-01-02 23:48:41 +0100

    Increate name length

--- event/models.py
+++ event/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class TradeSuiteEvent(models.Model):
-    name = models.CharField(max_length=30, default="Automatic trade event")
+    name = models.CharField(max_length=100, default="Automatic trade event")
     custom_full = models.CharField(max_length=50, default='')
     date = models.CharField(max_length=50, default='')
     time_started = models.CharField(max_length=50, default='')
